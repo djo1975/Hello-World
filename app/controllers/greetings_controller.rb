0f6f0b5e0eb743892c61cf9685511ade43ebbd94
djo1975 <mladanilic1975@gmail.com>
2023-06-13 13:10:50 +0200

    Linter errors fix

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,14 +1,13 @@
 class GreetingsController < ApplicationController
-    def random
-      random_greeting = Message.order(Arel.sql('RANDOM()')).first
-      render json: { greeting: random_greeting&.content }
-    end
-  
-    def random_batch
-      count = params[:count].to_i
-      random_greetings = Message.order(Arel.sql('RANDOM()')).limit(count)
-      greetings = random_greetings.map(&:content)
-      render json: { greetings: greetings }
-    end
+  def random
+    random_greeting = Message.order(Arel.sql('RANDOM()')).first
+    render json: { greeting: random_greeting&.content }
   end
-  
+
+  def random_batch
+    count = params[:count].to_i
+    random_greetings = Message.order(Arel.sql('RANDOM()')).limit(count)
+    greetings = random_greetings.map(&:content)
+    render json: { greetings: }
+  end
+end
